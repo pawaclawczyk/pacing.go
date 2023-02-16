21dd1cd60e6d897a3753e658a46af3a3f67686f6
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2023-02-16 15:16:06 +0100

    Convert time to slot

--- pacing/workload.go
+++ pacing/workload.go
@@ -2,7 +2,9 @@ package pacing
 
 import (
 	"github.com/google/uuid"
+	"math"
 	"sync"
+	"time"
 )
 
 type PlannedSpend struct {
@@ -28,6 +30,25 @@ func (s *PlannedSpend) Load(path string) error {
 		s.ps[rec.LineItemID] = EvenDistribution(rec.DailyBudget)
 	}
 	return nil
+}
+
+type Spend struct {
+	mu sync.RWMutex
+	s  map[uuid.UUID]int64
+}
+
+func NewSpend() *Spend {
+	return &Spend{
+		mu: sync.RWMutex{},
+		s:  map[uuid.UUID]int64{},
+	}
+}
+
+func CurrentSlot(t time.Time) int {
+	y, m, d := t.Date()
+	a := time.Date(y, m, d, 0, 0, 0, 0, t.Location())
+	secs := t.Sub(a).Seconds()
+	return int(math.Floor(secs / 60))
 }
 
 func DistributeBudgetFragments(bidders []string) interface{} {
--- pacing/workload_test.go
+++ pacing/workload_test.go
@@ -3,6 +3,7 @@ package pacing
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestPlannedSpendLoad(t *testing.T) {
@@ -17,5 +18,26 @@ func TestPlannedSpendLoad(t *testing.T) {
 		v, ok := s.ps[rec.LineItemID]
 		assert.True(t, ok)
 		assert.Equal(t, rec.DailyBudget, Sum(v))
+	}
+}
+
+func TestCurrentSlot(t *testing.T) {
+	type args struct {
+		t time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"zero", args{time.Date(2023, 2, 16, 0, 0, 0, 0, time.Local)}, 0},
+		{"ending", args{time.Date(2023, 2, 16, 0, 0, 59, 0, time.Local)}, 0},
+		{"beginning", args{time.Date(2023, 2, 16, 0, 1, 0, 0, time.Local)}, 1},
+		{"last", args{time.Date(2023, 2, 16, 23, 59, 59, 0, time.Local)}, 1439},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, CurrentSlot(tt.args.t), "CurrentSlot(%v)", tt.args.t)
+		})
 	}
 }
