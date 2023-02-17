597297ccb0daa3e456ee9afc13032be7c7050901
Paweł Wacławczyk <p.a.waclawczyk@gmail.com>
2023-02-17 14:48:09 +0100

    Split remaining budget - workload callback

--- pacing/workload.go
+++ pacing/workload.go
@@ -32,6 +32,16 @@ func (s *PlannedSpend) Load(path string) error {
 	return nil
 }
 
+func (s *PlannedSpend) Get(t int) map[uuid.UUID]int64 {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	res := make(map[uuid.UUID]int64)
+	for id, dist := range s.ps {
+		res[id] = dist[t]
+	}
+	return res
+}
+
 type Spend struct {
 	mu sync.RWMutex
 	s  map[uuid.UUID]int64
@@ -44,13 +54,43 @@ func NewSpend() *Spend {
 	}
 }
 
-func CurrentSlot(t time.Time) int {
+func (s *Spend) Get(id uuid.UUID) int64 {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	res, ok := s.s[id]
+	if !ok {
+		return int64(0)
+	}
+	return res
+}
+
+func TimeToSlot(t time.Time) int {
 	y, m, d := t.Date()
 	a := time.Date(y, m, d, 0, 0, 0, 0, t.Location())
 	secs := t.Sub(a).Seconds()
 	return int(math.Floor(secs / 60))
 }
 
-func DistributeBudgetFragments(bidders []string) interface{} {
-	return nil
+func MakeWorkloadSplitter(planned *PlannedSpend, spend *Spend, now func() time.Time) func(consumers []string) map[string]interface{} {
+	return func(consumers []string) map[string]interface{} {
+		if len(consumers) == 0 {
+			return map[string]interface{}{}
+		}
+		slot := TimeToSlot(now())
+		consWrk := map[uuid.UUID]int64{}
+		for id, planned := range planned.Get(slot) {
+			diff := planned - spend.Get(id)
+			// skip line item if the available budget will be 0 or less per consumer
+			if diff < int64(len(consumers)) {
+				continue
+			}
+			fragment := diff / int64(len(consumers))
+			consWrk[id] = fragment
+		}
+		wrk := map[string]interface{}{}
+		for _, c := range consumers {
+			wrk[c] = consWrk
+		}
+		return wrk
+	}
 }
--- pacing/workload_test.go
+++ pacing/workload_test.go
@@ -1,6 +1,7 @@
 package pacing
 
 import (
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -37,7 +38,131 @@ func TestCurrentSlot(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, CurrentSlot(tt.args.t), "CurrentSlot(%v)", tt.args.t)
+			assert.Equalf(t, tt.want, TimeToSlot(tt.args.t), "TimeToSlot(%v)", tt.args.t)
+		})
+	}
+}
+
+func TestMakeWorkloadSplitterReturnsValidNumberOfWorkloads(t *testing.T) {
+	planned := NewPlannedSpend()
+	spend := NewSpend()
+	now := func() time.Time { return time.Date(2023, 2, 17, 0, 0, 0, 0, time.Local) }
+	splitter := MakeWorkloadSplitter(planned, spend, now)
+	tests := []struct {
+		name string
+		args []string
+		want int
+	}{
+		{"zero consumers", []string{}, 0},
+		{"one consumers", []string{"alice"}, 1},
+		{"three consumers", []string{"alice", "bob", "charlie"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			split := splitter(tt.args)
+			assert.Len(t, split, tt.want)
+		})
+	}
+}
+
+func TestMakeWorkloadSplitterDividesWorkEqually(t *testing.T) {
+	planned := NewPlannedSpend()
+	lineItemId := uuid.New()
+	planned.ps = map[uuid.UUID][]int64{
+		lineItemId: {9},
+	}
+	spend := NewSpend()
+	now := func() time.Time { return time.Date(2023, 2, 17, 0, 0, 0, 0, time.Local) }
+	splitter := MakeWorkloadSplitter(planned, spend, now)
+	tests := []struct {
+		name string
+		args []string
+		want int64
+	}{
+		{"zero consumers", []string{}, 0},
+		{"one consumers", []string{"alice"}, 9},
+		{"two consumers", []string{"alice", "bob"}, 4},
+		{"three consumers", []string{"alice", "bob", "charlie"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			split := splitter(tt.args)
+			for k, v := range split {
+				assert.Contains(t, tt.args, k)
+				assert.Equal(t, v.(map[uuid.UUID]int64)[lineItemId], tt.want)
+			}
+		})
+	}
+}
+
+func TestMakeWorkloadSplitterTakesIntoAccountSpend(t *testing.T) {
+	planned := NewPlannedSpend()
+	lineItemId := uuid.New()
+	planned.ps = map[uuid.UUID][]int64{
+		lineItemId: {9},
+	}
+	spend := NewSpend()
+	now := func() time.Time { return time.Date(2023, 2, 17, 0, 0, 0, 0, time.Local) }
+	consumers := []string{"alice", "bob", "charlie"}
+	splitter := MakeWorkloadSplitter(planned, spend, now)
+	tests := []struct {
+		name string
+		args int64
+		want int64
+	}{
+		{"no spend", 0, 3},
+		{"partial spend", 6, 1},
+		{"partial spend not enough for all", 7, 0},
+		{"full spend", 9, 0},
+		{"overspend", 10, 0},
+	}
+	for _, tt := range tests {
+		spend.s[lineItemId] = tt.args
+		t.Run(tt.name, func(t *testing.T) {
+			split := splitter(consumers)
+			assert.Len(t, split, 3)
+			for _, v := range split {
+				vv, ok := v.(map[uuid.UUID]int64)[lineItemId]
+				// Expect that items without budget are not distributed
+				if tt.want > 0 {
+					assert.True(t, ok)
+					assert.Equal(t, vv, tt.want)
+				} else {
+					assert.False(t, ok)
+				}
+			}
+		})
+	}
+}
+
+func TestMakeWorkloadSplitterComputesForSpecificSlot(t *testing.T) {
+	planned := NewPlannedSpend()
+	lineItemId := uuid.New()
+	planned.ps = map[uuid.UUID][]int64{
+		lineItemId: {9, 18, 27},
+	}
+	spend := NewSpend()
+	consumers := []string{"alice", "bob", "charlie"}
+	tests := []struct {
+		name string
+		args int
+		want int64
+	}{
+		{"time slot 0", 0, 3},
+		{"time slot 1", 1, 6},
+		{"time slot 2", 2, 9},
+	}
+	for _, tt := range tests {
+		now := func() time.Time { return time.Date(2023, 2, 17, 0, tt.args, 0, 0, time.Local) }
+		splitter := MakeWorkloadSplitter(planned, spend, now)
+		t.Run(tt.name, func(t *testing.T) {
+			split := splitter(consumers)
+			assert.Len(t, split, 3)
+			for _, v := range split {
+				vv, ok := v.(map[uuid.UUID]int64)[lineItemId]
+				assert.True(t, ok)
+				assert.Equal(t, vv, tt.want)
+			}
 		})
 	}
 }
